9568882435b2276a20919444b410670af9b0d33a
lukaslangede <lukas@lukaslange.de>
2021-01-27 11:13:36 -0800

    Anpassen der Kommentare

--- Delete-RAWs.ps1
+++ Delete-RAWs.ps1
@@ -1,5 +1,5 @@
 foreach ($raw in (Get-Item *.cr3))
-{# Durchlaufen aller .cr2 Dateien
+{# Durchlaufen aller .cr3 Dateien
 
     if (!(Test-Path "$(($raw.Name).split(".")[0]).jpg"))
     {# Wenn kein passendes .jpg zum aktuellen Raw vorhanden ist, lösche das Raw
